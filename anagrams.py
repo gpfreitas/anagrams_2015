c38819cff4ff9a7383354389eed4390fe50430d8
Guilherme Pereira de Freitas <gpfreitas@yahoo-inc.com>
2015-08-26 02:13:21 -0700

    Comments on a simple design decision.

--- anagrams.py
+++ anagrams.py
@@ -98,6 +98,12 @@ if __name__ == "__main__":
         # We can rule out words that do not have the same length as target_word
         target_length = len(args.target_word)
         candidate_words = (w for w in words if len(w) == target_length)
+        # Better keep filters like the one above in candidate_words here rather
+        # than inside the anagram_checker function. For example, a user might
+        # know that the word list at hand has only words with the same length.
+        # In that case, it is wasteful to verify the lengths of the words. It
+        # seems to me that the most natural place for modifications in such
+        # cases is in the script, not in the innards of a function.
         anagrams = filter(is_anagram, candidate_words)
         for word in anagrams:
             print(word)
